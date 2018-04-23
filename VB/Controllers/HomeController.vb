05ada3a0837dd853f9b4f62752e926b3c9b2767c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/Controllers/HomeController.vb
@@ -0,0 +1,71 @@
+﻿Imports System
+Imports System.IO
+Imports System.Web
+Imports System.Web.Mvc
+Imports DevExpress.XtraPrinting
+Imports DevExpress.XtraPrintingLinks
+Imports DevExpress.Web.Mvc
+
+Public Class HomeController
+    Inherits Controller
+
+    Public Function Index() As ActionResult
+        Return View(New MyViewModel With {.Products = MyModel.GetProducts()})
+    End Function
+
+    Public Function GridViewPartialProducts() As ActionResult
+        Return PartialView(MyModel.GetProducts())
+    End Function
+
+    Public Function ExportTo() As ActionResult
+      Return GridViewExtension.ExportToXlsx(GridViewHelper.ExportGridViewSettings, MyModel.GetProducts())
+
+    End Function
+End Class
+
+Public NotInheritable Class GridViewHelper
+    Private Sub New()
+    End Sub
+    Private Shared m_exportGridViewSettings As GridViewSettings
+
+    Public Shared ReadOnly Property ExportGridViewSettings() As GridViewSettings
+        Get
+            If m_exportGridViewSettings Is Nothing Then
+                m_exportGridViewSettings = CreateExportGridViewSettings()
+            End If
+            Return m_exportGridViewSettings
+        End Get
+    End Property
+
+    Private Shared Function CreateExportGridViewSettings() As GridViewSettings
+        Dim settings As New GridViewSettings()
+
+        settings.Name = "gvProducts"
+        settings.CallbackRouteValues = New With { _
+         Key .Controller = "Home", _
+         Key .Action = "GridViewPartialProducts" _
+        }
+
+        settings.KeyFieldName = "ProductID"
+        settings.Settings.ShowFilterRow = True
+
+        settings.Columns.Add("ProductID")
+        settings.Columns.Add("ProductName")
+        settings.Columns.Add("UnitPrice")
+
+        settings.SettingsExport.RenderBrick = Function(sender, e)
+                                                  If e.RowType <> GridViewRowType.Data Then
+                                                      Exit Function
+                                                  End If
+                                                  If TryCast(e.Column, GridViewDataColumn).FieldName = "UnitPrice" AndAlso e.RowType <> GridViewRowType.Header Then
+                                                      If Convert.ToInt32(e.TextValue) > 15 Then
+                                                          e.BrickStyle.BackColor = System.Drawing.Color.Yellow
+                                                      Else
+                                                          e.BrickStyle.BackColor = System.Drawing.Color.Green
+                                                      End If
+                                                  End If
+                                              End Function
+
+        Return settings
+    End Function
+End Class
---
+++ VB/Models/Model.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Web
+Imports System.Data
+Imports System.Data.OleDb
+Imports System.Collections.Generic
+
+Public Class MyModel
+
+    Public Shared Function GetProducts() As DataTable
+        Dim dataTableProducts As New DataTable()
+        Using connection As OleDbConnection = GetConnection()
+            Dim adapter As New OleDbDataAdapter(String.Empty, connection)
+            adapter.SelectCommand.CommandText = "SELECT [ProductID], [ProductName], [CategoryID], [UnitPrice] FROM [Products]"
+            adapter.Fill(dataTableProducts)
+        End Using
+        Return dataTableProducts
+    End Function
+
+    Private Shared Function GetConnection() As OleDbConnection
+        Dim connection As New OleDbConnection()
+        connection.ConnectionString = String.Format("Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0}", HttpContext.Current.Server.MapPath("~/App_Data/nwind.mdb"))
+        Return connection
+    End Function
+
+End Class
+
+Public Class MyViewModel
+
+    Private privateProducts As DataTable
+    Public Property Products() As DataTable
+        Get
+            Return privateProducts
+        End Get
+        Set(ByVal value As DataTable)
+            privateProducts = value
+        End Set
+    End Property
+End Class
